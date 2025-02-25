bd37152c8cb2f221085bc9333503e5f806ff427a
uzinsea@gmail.com <uzinsea@gmail.com>
2025-02-25 23:25:28 +0900

    변경

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,9 +1,7 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api") //localhost:8080/api
@@ -15,7 +13,21 @@ public class GetController {
     }
 
     @GetMapping(value = "/getParameter") //localhost:8080/api/getParameter?id=1234&password=abcd
-    public String getParameter(String id, String password) {
-        return "id: " + id + ", password: " + password;
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd) {
+        System.out.println(id);
+        System.out.println(pwd);
+        return "id: " + id + ", password: " + pwd;
+    }
+
+    @GetMapping(value = "/getMultiParameter")
+//    public String getMultiParameter(SearchParam searchParam){
+    public SearchParam getMultiParameter(SearchParam searchParam) {
+
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+//        return "OK";
+        return searchParam;
     }
 }
